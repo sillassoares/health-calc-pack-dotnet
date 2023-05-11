dde741fc4b35a77fe822c2843205743dd6c9807f
Sillas Soares <sillas@legalboards.io>
2023-05-11 20:49:47 -0300

    initial commit - arquitetura executavel

---
+++ health-calc-pack-dotnet/health-calc-pack-dotnet-test/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace health_calc_pack_dotnet_test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ health-calc-pack-dotnet/health-calc-pack-dotnet/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace health_calc_pack_dotnet;
+public class Class1
+{
+
+}
+
